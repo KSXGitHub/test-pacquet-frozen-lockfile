43db9788b75a22558f48bd9f240deee8720c9ac0
khai96_ <hvksmr1996@gmail.com>
2023-08-16 13:30:42 +0700

    add pacquet install

--- install.bash
+++ install.bash
@@ -12,3 +12,5 @@ export PATH="$HOME/programming/pacquet/target/release:$PATH"
 pretty-exec -- rm -rf pacquet-home node_modules pacquet-lock.yaml
 
 pretty-exec -- which pacquet
+
+pretty-exec -- pacquet install

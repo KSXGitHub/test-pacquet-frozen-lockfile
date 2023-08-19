f5619e96f7179afe663c332e1dc9bf59f37faa48
khai96_ <hvksmr1996@gmail.com>
2023-08-19 12:32:00 +0700

    remove useless quotes

--- install.bash
+++ install.bash
@@ -2,11 +2,11 @@
 set -o errexit -o pipefail -o nounset
 cd "$(dirname "$0")"
 
-PACQUET_HOME="$(pwd)/pacquet-home"
+PACQUET_HOME=$(pwd)/pacquet-home
 export PACQUET_HOME
 export TRACE=${TRACE:-pacquet=info}
 export RUST_BACKTRACE=1
-export PATH="$HOME/programming/pacquet/target/debug:$PATH"
+export PATH=$HOME/programming/pacquet/target/debug:$PATH
 
 pretty-exec -- run-at "$HOME/programming/pacquet" cargo build
 pretty-exec -- rm -rf pacquet-home node_modules pacquet-lock.yaml

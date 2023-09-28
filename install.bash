e14221b24c066cafd180d83c895527b43b144561
khai96_ <hvksmr1996@gmail.com>
2023-09-28 21:51:33 +0700

    use time to measure time

--- install.bash
+++ install.bash
@@ -10,4 +10,4 @@ export PATH=$HOME/programming/pacquet/target/debug:$PATH
 pretty-exec -- run-at "$HOME/programming/pacquet" cargo build
 pretty-exec -- rm -rf pacquet-home node_modules
 pretty-exec -- which pacquet
-pretty-exec -- pacquet install
+pretty-exec -- time pacquet install

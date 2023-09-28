10a62747dbe66f9712e11297f58a0aace84f7334
khai96_ <hvksmr1996@gmail.com>
2023-09-28 21:31:07 +0700

    enable lockfile

--- install.bash
+++ install.bash
@@ -8,6 +8,6 @@ export RUST_BACKTRACE=1
 export PATH=$HOME/programming/pacquet/target/debug:$PATH
 
 pretty-exec -- run-at "$HOME/programming/pacquet" cargo build
-pretty-exec -- rm -rf pacquet-home node_modules pacquet-lock.yaml
+pretty-exec -- rm -rf pacquet-home node_modules
 pretty-exec -- which pacquet
 pretty-exec -- pacquet install

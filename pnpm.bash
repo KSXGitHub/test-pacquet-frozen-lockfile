496b4d989de10e89365f9daa08a83729c4a34811
khai96_ <hvksmr1996@gmail.com>
2023-09-29 16:19:58 +0700

    create script for pnpm

---
+++ pnpm.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -o errexit -o pipefail -o nounset
+cd "$(dirname "$0")"
+
+PNPM_HOME=$(pwd)/pnpm-home
+export PNPM_HOME
+
+pretty-exec -- rm -rf pnpm-home node_modules
+pretty-exec -- which pnpm
+pretty-exec -- pnpm -v
+pretty-exec -- bash -c 'time pnpm install'

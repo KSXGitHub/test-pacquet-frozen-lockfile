454375edf4fccd16606c40674cf27140538b39c7
khai96_ <hvksmr1996@gmail.com>
2023-09-29 17:21:52 +0700

    create script for release build

---
+++ pacquet-release.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -o errexit -o pipefail -o nounset
+cd "$(dirname "$0")"
+
+PACQUET_HOME=$(pwd)/pacquet-home
+export PACQUET_HOME
+export RUST_BACKTRACE=1
+export PATH=$HOME/programming/pacquet/target/release:$PATH
+
+pretty-exec -- run-at "$HOME/programming/pacquet" cargo build --release
+pretty-exec -- rm -rf pacquet-home node_modules
+pretty-exec -- which pacquet
+pretty-exec -- bash -c 'time pacquet install'
